5103d413e1b3e7a94da5d9df6218b0094e15a12b
Diego Enrique Hernández Cholotío <104159636+unhumanosinmas@users.noreply.github.com>
2022-04-23 15:47:18 -0600

    Cambio del fondo
    Se creó todo lo relacionado a lo minimo necesario para realizar el cambio del color de fondo

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
+import React from 'react';
+import { getColores } from './colors';
+
 
 function App() {
+
+let [nombreColor, setNombreColor] = React.useState("white")
+let allColors = getColores();
+
+function GetColor(){
+  var numero = Math.floor(Math.random()*allColors.length)
+  var color = allColors[numero][0]
+  setNombreColor (allColors[numero][1])
+
+  document.body.style.backgroundColor = `#${color}`;
+  document.class.card.style.backgroundColor = `#${color}`;
+
+}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="grid__item">
+        <div class="card" >
+          <div class="card" align="center">
+          </div>
+          <div class="card__content">
+            <h1 class="card__header">El color de fondo es: {nombreColor}</h1>
+            <button class="card__btn" onClick={()=>{GetColor()}}>Cambiar Color</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
